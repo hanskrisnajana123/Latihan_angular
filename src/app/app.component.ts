e38f34cbe053be01fddae1a115c25f956dadfdf5
hanskrisnajana123 <hanskrisnajana@ymail.com>
2020-04-16 20:25:34 +0700

    2 files modified - final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  hasil : number;
+  Bintang = ""
+  Bintang1 = ""
+  Bulat = ""
+  bilArr1 = []
+  bilArr2 = []
+  pola = []
+  pola1 = []
+  bray : number;
+  findResult(a, b){
+    this.hasil = (Math.pow(parseInt(a),parseInt(b)))
+    this.bilArr1.push(a)
+    this.bilArr2.push(b)
+  
+    if(this.hasil % 2 == 0){
+      for(var i=parseInt(b) - 1;i<(this.hasil);i++){
+        for(var j=0;j<=i;j++){
+          this.Bintang = this.Bintang + "*"
+        }
+        this.pola.push(this.Bintang)
+        this.Bintang = ""
+      }
+    }
+    else{
+
+      for(var j=0;j<=5;j++){
+          this.Bintang1 = ""
+          this.Bulat = ""
+        for(var i=0;i<parseInt(b);i++){
+          this.Bintang1 = this.Bintang1 + "*"
+        }
+        for(var i=parseInt(a);i>0;i--){
+          this.Bulat = this.Bulat + "o"
+        }
+        this.pola1.push(this.Bintang1 + this.Bulat)
+        b = parseInt(b) - 1
+        a = parseInt(a) - 1
+      }
+    }
+
+    
+  }
+
+
 }
